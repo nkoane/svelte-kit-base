0aedad61cdb3ef5a5831aa5c2d4b90407cb78ff2
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-03-02 12:09:30 +0200

    okay, this should be it

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
     import '../app.postcss';
 </script>
 
-<slot />
+<svelte:head>
+    <title>S:KIT</title>
+</svelte:head>
+<main
+    class="mx-auto mt-8 w-10/12 rounded-md bg-sky-200 p-4 sm:bg-gray-300 md:bg-teal-400 lg:bg-rose-200">
+    <slot />
+</main>
