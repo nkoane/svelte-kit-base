ed19fa24c1a6e45d084643621f85f9d04f774d2d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-03-02 14:41:48 +0200

    i need, icons to work, but lets see how far we got

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
-<script>
+<script lang="ts">
     import '../app.postcss';
+    import '/fortawesome/fontawesome-free/css/all.css';
 </script>
 
 <svelte:head>
     <title>S:KIT</title>
 </svelte:head>
 <main
-    class="mx-auto mt-8 w-10/12 rounded-md bg-sky-200 p-4 sm:bg-gray-300 md:bg-teal-400 lg:bg-rose-200">
+    class="mx-auto mt-8 w-10/12 rounded-md bg-sky-200 p-4 sm:bg-gray-300 md:bg-teal-400 lg:bg-lime-500">
     <slot />
 </main>
