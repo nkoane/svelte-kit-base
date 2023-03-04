2ed70f0e3a3e80a39448a2874809aecf3d62af98
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-03-04 10:59:25 +0200

    i think we found our match

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import '../app.postcss';
-    import {
-        AddressBookRegular,
-        ArrowDownUpLockSolid,
-        BasketShoppingSolid
-    } from 'svelte-awesome-icons';
+    // const feather = require('feather-icons');
+    import * as feather from 'feather-icons';
+    import { onMount } from 'svelte';
+    // import feather from './../../node_modules/feather-icons/dist/feather.min.js';
+    // ('node_modules/feather-icons/dist/feather.min.js');
+    onMount(() => feather.replace());
 </script>
 
 <svelte:head>
@@ -12,9 +13,6 @@
 </svelte:head>
 <main
     class="mx-auto mt-8 w-10/12 rounded-md bg-sky-200 p-4 sm:bg-gray-300 md:bg-teal-400 lg:bg-lime-500">
-    <i class="fa-regular fa-lightbulb" />
-    <AddressBookRegular />
-    <ArrowDownUpLockSolid />
-    <BasketShoppingSolid size="100" color="#fff" />
+    <i data-feather="video" color="#fff" class="h-16 w-16" />
     <slot />
 </main>
