b6b66c523c96d96051c045e26aa5ab127e83cd3d
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2023-03-15 12:31:21 +0200

    yeah, yeah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-    import type { PageData, ActionData } from './$types';
-    import Register from '$lib/form/Register.svelte';
-
-    export let data: PageData;
-    export let form: ActionData;
-
-    console.log(data, form);
-</script>
-
-<Register {form} />
+<p>And, we are here.</p>
